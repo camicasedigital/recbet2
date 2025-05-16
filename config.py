e77d6fe8d31881a047827832a451f0db017092a6
camicasedigital <agenciacamicase@gmail.com>
2025-05-15 22:14:20 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL').replace("mysql://", "mysql+pymysql://")
+    url = os.getenv('DATABASE_URL')
+    if not url:
+        raise RuntimeError("❌ DATABASE_URL não está configurada no ambiente.")
+    
+    SQLALCHEMY_DATABASE_URI = url.replace("mysql://", "mysql+pymysql://")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
